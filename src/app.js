f3f77ec8148c07d3c740a54697dc50bcfd2ddb42
Cristian Trapiello <119610379+CrisMader@users.noreply.github.com>
2025-10-31 10:56:31 +0000

    Ejercicio Base Hecho

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['camiones', 'capcom', 'midu', 'griterio', 'internet'];
+let domains = ['com', 'es', 'dev', 'io', 'net']
+
+
+const dominioGenerator = () =>{
+  let dominioFinal =[]
+  for(let pro of pronoun){
+    for(let a of adj){
+      for(let no of noun){
+        for(let dom of domains){
+          dominioFinal.push(`${pro}${a}${no}.${dom}`);
+        }
+      }
+    }
+  }
+
+  return dominioFinal;
+}
+
+console.log(dominioGenerator())
+
+
+
+window.onload = function () {
+
 };
+
+
